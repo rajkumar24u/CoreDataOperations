e7a799f465d76653cbed3ed62fdcc17bf0237108
sunarc <sunarc@mac-mini.local>
2016-11-08 11:27:07 +0530

    Initial Commit

---
+++ CoreDataRecords/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataRecords
+//
+//  Created by sunarc on 08/11/16.
+//  Copyright © 2016 sunarc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
